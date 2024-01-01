70aaf9e633e39a627a6e172cf682e6d5b16427f4
mgmetehan <mgmetehan@hotmail.com>
2024-01-01 15:13:43 +0300

    init project

---
+++ src/main/java/com/mgmetehan/streamexample/StreamExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.streamexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/streamexample/StreamExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.streamexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
